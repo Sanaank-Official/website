a347f0795f95fe8d2db5e146506bba748317a70e
qaidjoharj53 <qaidjoharytqt@gmail.com>
2024-12-08 15:13:14 +0530

    updates

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { CheckCircle, Target, Trophy } from "lucide-react";
 
-const About = () => {
+export default function About() {
 	return (
 		<div className="max-w-full bg-gray-50">
 			{/* 1. Hero Section */}
@@ -17,16 +17,16 @@ const About = () => {
 				}}>
 				<div className="container mx-auto text-center">
 					<motion.h1
-						initial={{ opacity: 0, y: -50 }}
-						animate={{ opacity: 1, y: 10 }}
+						initial={{ opacity: 0, x: -100 }}
+						animate={{ opacity: 1, x: 0 }}
 						transition={{ duration: 1 }}
 						className="text-5xl text-white font-bold drop-shadow-md">
 						About Us
 					</motion.h1>
 					<motion.p
-						initial={{ opacity: 0, y: 20 }}
-						animate={{ opacity: 1, y: 0 }}
-						transition={{ duration: 1, delay: 0.3 }}
+						initial={{ opacity: 0, x: 100 }}
+						animate={{ opacity: 1, x: 0 }}
+						transition={{ duration: 1 }}
 						className="text-xl text-black font-medium pt-8">
 						Get to know us better
 					</motion.p>
@@ -189,6 +189,4 @@ const About = () => {
 			</section>
 		</div>
 	);
-};
-
-export default About;
+}
